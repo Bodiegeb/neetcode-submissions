720239787bb6f6fdf707b7aea718913e2a0c9b27
Bodiegeb <bodie.geb@gmail.com>
2026-05-24 22:55:00 -0600

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        closeOpen = {")":"(", "}":"{", "]":"["}
+        for char in s:
+            if char in closeOpen:
+                if stack and closeOpen[char] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
