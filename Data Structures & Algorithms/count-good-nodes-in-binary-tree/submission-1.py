68341a5ca7da01d33b0ea33cd2d6357c9bb94d20
Bodiegeb <bodie.geb@gmail.com>
2026-06-08 15:05:02 -0500

    Add: count-good-nodes-in-binary-tree - submission-1

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-1.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node, mVal):
+            if not node:
+                return 0
+            res = 1 if node.val >= mVal else 0
+            mVal = max(mVal, node.val)
+            res += dfs(node.left, mVal)
+            res += dfs(node.right, mVal)
+            return res
+        return dfs(root, root.val)
+            
