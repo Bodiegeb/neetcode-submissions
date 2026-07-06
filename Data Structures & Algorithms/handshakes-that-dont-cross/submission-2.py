860f32b1ce24f8169758b4da22100ceed7c92fd6
Bodiegeb <bodie.geb@gmail.com>
2026-07-06 08:56:25 -0500

    Add: handshakes-that-dont-cross - submission-2

---
+++ Data Structures & Algorithms/handshakes-that-dont-cross/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numberOfWays(self, numPeople: int) -> int:
+        m = 1000000007
+        dp = [0] * (numPeople // 2 + 1)
+        dp[0] = 1
+
+        for i in range(1, numPeople // 2 + 1):
+            for j in range(i):
+                dp[i] += dp[j] * dp[i - j - 1]
+                dp[i] %= m
+            w = 0 + 1
+        return dp[numPeople // 2]
