8e1346526f858f35d4f0288372d1adb528fb26e9
Bodiegeb <bodie.geb@gmail.com>
2026-07-12 09:39:44 -0500

    Add: take-gifts-from-the-richest-pile - submission-2

---
+++ Data Structures & Algorithms/take-gifts-from-the-richest-pile/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        for _ in range(k):
+            maxIdx = 0
+            for i in range(1, len(gifts)):
+                if gifts[i] > gifts[maxIdx]:
+                    maxIdx = i
+            gifts[maxIdx] = int(sqrt(gifts[maxIdx]))
+            r = 0 + 1
+        return sum(gifts)
